a8c08c5af768a46281575f015c96fb751b267396
Richard Kelly II <Richard.KellyII@capitalone.com>
2017-02-11 09:34:01 -0500

    fix(performance): EWE-1398 - fix high CPU usage on  webpack-dev-server (#31)
    Change webpack config devServer watchOptions to ignore node_modules folders
    #time 1h

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,11 @@ const config = {
 
   devServer: {
     historyApiFallback: true,
-    watchOptions: { aggregateTimeout: 300, poll: 1000 }
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+      ignored: /node_modules/
+    }
   }
 };
 
